b2d364ff667a3927325fa833f921a1e425fe55ae
Alekksandr Fikov <cupofcoffee1979@gmail.com>
2024-11-11 11:07:27 +0300

    Add maven project files

---
+++ src/main/java/ProgectStarter.java
@@ -0,0 +1,15 @@
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ProgectStarter {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new AnnotationConfigApplicationContext();
+		
+		
+
+	}
+
+}
